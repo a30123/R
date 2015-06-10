d2e46f35f54d547a05dc562503253a74d451f245
Yu-Ru Huang <mary@linshunghuang.com>
2015-06-10 22:23:27 +0800

    hu

--- try_FSelector_correlation.R
+++ try_FSelector_correlation.R
@@ -1,5 +1,5 @@
 library(FSelector)
-my_data_path<-"C:/Users/A30123.ITRI/Documents/R scripts/New for event mining/Try_20150607_FSelector/correlation/input_features_15cycles.csv"
+my_data_path<-"C:/Users/A30123.ITRI/Documents/R scripts/New for event mining/Try_20150607_FSelector/correlation/input_38features_15cycles.csv"
 my_data_path2<-"C:/Users/A30123.ITRI/Documents/R scripts/New for event mining/Try_20150607_FSelector/correlation/Target_dP_Filter_max_15cycles.csv"
 
 my_data2<-read.csv(my_data_path)
@@ -8,7 +8,7 @@ my_data3<-read.csv(my_data_path2)
 my_data<-cbind(my_data2,my_data3)
 weights<-linear.correlation(Target~., my_data)
 print(weights)
-write.csv(weights,"C:/Users/A30123.ITRI/Documents/R scripts/New for event mining/Try_20150607_FSelector/correlation/Correlation result.csv",row.names = T)
+write.csv(weights,"C:/Users/A30123.ITRI/Documents/R scripts/New for event mining/Try_20150607_FSelector/correlation/Correlation result_38features.csv",row.names = T)
 
 weights2<-rank.correlation(Target~., my_data)
-write.csv(weights2,"C:/Users/A30123.ITRI/Documents/R scripts/New for event mining/Try_20150607_FSelector/correlation/Spearmans Correlation result.csv",row.names = T)
+write.csv(weights2,"C:/Users/A30123.ITRI/Documents/R scripts/New for event mining/Try_20150607_FSelector/correlation/Spearmans Correlation result_713features.csv",row.names = T)
