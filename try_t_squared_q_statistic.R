c5160a992dec62528687313fdaa2d4420ffc6a61
Yu-Ru Huang <mary@linshunghuang.com>
2015-07-02 18:25:36 +0800

    coco

--- try_t_squared_q_statistic.R
+++ try_t_squared_q_statistic.R
@@ -28,14 +28,33 @@ my_data_path<-"C:/Users/A30123.ITRI/Documents/R scripts/New for event mining/Try
 #########################################################################################################
 #######################################   MAIN PROGRAM        ###########################################
 #########################################################################################################
+#Read in the data
 all_data<-read.csv(my_data_path)
+
+#extract only first 200 entries
 my_data<-all_data[1:200,]
 
+my_data2<-all_data[1:200,1:10]
+pairs(my_data2)
 #standardize each column
 standardized_my_data<-scale(my_data,center=TRUE,scale=TRUE)
 
 coco<-matrix(standardized_my_data,length(my_data[,1]))
 #coco<-as.matrix(standardized_my_data)
 
-t(coco)%*%coco
+cov<-t(coco)%*%coco
 
+eigs<-eigen(cov,symmetric=TRUE,only.values=FALSE)
+
+
+############### PCA
+### watch this:https://www.youtube.com/watch?v=Heh7Nv4qimU
+
+pca_result<-princomp(my_data,scores=TRUE,cor=TRUE)
+plot(pca_result)
+biplot(pca_result)
+pca_result$loadings
+pca_result$scores
+############## PCA on non-standardized data is exactly the same
+pca_result2<-princomp(standardized_my_data,scores=TRUE,cor=TRUE)
+plot(pca_result2)
